8413694a0cb04e4af7bd0950ac638d5a370aa97d
QuizitoCristiano <quizitocristiano10@gmail.com>
2023-05-27 22:19:08 -0300

    colocando o menu no site

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const menuIcon = document.querySelector('#menu-icon');
-const openIcons = document.querySelector('.openIcons');
-const closeIcons = document.querySelector('.closeIcons');
+// const menuIcon = document.querySelector('#menu-icon');
+// const openIcons = document.querySelector('.openIcons');
+// const closeIcons = document.querySelector('.closeIcons');
 
 //  const entrarNoZap = () => {
-    
-  
+
+
 //  }
 
 
@@ -22,13 +22,47 @@ const closeIcons = document.querySelector('.closeIcons');
 
 
 
-const OpenIcons = () => {
-    openIcons.style.display = "none"
-    closeIcons.style.display = 'flex'
-    document.querySelector(".navbar").style.display = 'flex'
-}
-const CloseIcons = () => {
-    openIcons.style.display = "flex"
-    closeIcons.style.display = 'none'
-    document.querySelector(".navbar").style.display = 'none'
+// const OpenIcons = () => {
+//     openIcons.style.display = "none"
+//     closeIcons.style.display = 'flex'
+//     document.querySelector(".navbar").style.display = 'flex'
+// }
+// const CloseIcons = () => {
+//     openIcons.style.display = "flex"
+//     closeIcons.style.display = 'none'
+//     document.querySelector(".navbar").style.display = 'none'
+// }
+
+// function toggleMenu() {
+//     var menuIcon = document.getElementById("menuIcon");
+
+//     if (menuIcon.classList.contains("fa-bars")) {
+//         menuIcon.classList.remove("fa-bars");
+//         menuIcon.classList.add("fa-times");
+//     } else {
+//         menuIcon.classList.remove("fa-times");
+//         menuIcon.classList.add("fa-bars");
+//     }
+// }
+const linksDiv = document.querySelector(".navlinks");
+const openMenu = document.querySelector('.bx-menu')
+const fecharMenu = document.querySelector(".bx-tada")
+openMenu.style.display = 'flex'
+fecharMenu.style.display = 'none'
+
+function CloseIcons() {
+
+
+    if (linksDiv.style.display === "none") {
+        linksDiv.style.display = "block";
+        fecharMenu.style.display = 'flex'
+        openMenu.style.display = 'none'
+
+    } else {
+        linksDiv.style.display = "none";
+        fecharMenu.style.display = 'none'
+        openMenu.style.display = 'flex'
+    }
+
+
 }
