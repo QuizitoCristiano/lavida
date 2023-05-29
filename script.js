e319c0cfaa8553825f80a5a0e18a31327c8ba941
QuizitoCristiano <quizitocristiano10@gmail.com>
2023-05-28 23:46:10 -0300

    validando os campos do formulário

--- script.js
+++ script.js
@@ -1,49 +1,4 @@
-// const menuIcon = document.querySelector('#menu-icon');
-// const openIcons = document.querySelector('.openIcons');
-// const closeIcons = document.querySelector('.closeIcons');
 
-//  const entrarNoZap = () => {
-
-
-//  }
-
-
-// menuIcon.onclick = () => {
-//     menuIcon.classList.toggle('bx-x');
-//     navebar.classList.toggle('active');
-// }
-
-// const header = document.querySelector('header');
-
-// header.classList.toggle('sticky', window.scrollY > 100);
-
-// 
-// navebar.classList.remove('active')
-
-
-
-// const OpenIcons = () => {
-//     openIcons.style.display = "none"
-//     closeIcons.style.display = 'flex'
-//     document.querySelector(".navbar").style.display = 'flex'
-// }
-// const CloseIcons = () => {
-//     openIcons.style.display = "flex"
-//     closeIcons.style.display = 'none'
-//     document.querySelector(".navbar").style.display = 'none'
-// }
-
-// function toggleMenu() {
-//     var menuIcon = document.getElementById("menuIcon");
-
-//     if (menuIcon.classList.contains("fa-bars")) {
-//         menuIcon.classList.remove("fa-bars");
-//         menuIcon.classList.add("fa-times");
-//     } else {
-//         menuIcon.classList.remove("fa-times");
-//         menuIcon.classList.add("fa-bars");
-//     }
-// }
 const linksDiv = document.querySelector(".navlinks");
 const openMenu = document.querySelector('.bx-menu')
 const fecharMenu = document.querySelector(".bx-tada")
@@ -65,4 +20,128 @@ function CloseIcons() {
     }
 
 
+}
+
+
+
+
+
+function validaForm() {
+
+    validarCampoNome();
+    validarFormatoEmail();
+    validarCelular(celular);
+   
+    validarEndereco();
+    validarPassword();
+    validarConfirmPassword();
+}
+
+
+
+
+
+function validarCampoNome() {
+    const nomeInput = document.getElementById("nome");
+    const mensagemElement = document.getElementById("mensagem");
+    const nome = nomeInput.value;
+
+    if (nome === "") {
+        mensagemElement.innerHTML = "Por favor, preencha o campo de  Nome.";
+        mensagemElement.style.color = "red";
+    } else {
+        mensagemElement.innerHTML = "Dados enviados com sucesso!";
+        mensagemElement.style.color = "green";
+
+    }
+}
+
+
+function validarFormatoEmail() {
+    const emailInput = document.getElementById("email");
+    const myemail = document.getElementById("sms");
+    const email = emailInput.value;
+
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!emailRegex.test(email)) {
+        myemail.innerHTML = "Por favor, digite um e-mail válido.";
+        myemail.style.color = "red";
+    } else {
+        myemail.innerHTML = "Dados enviados com sucesso!";
+        myemail.style.color = "green";
+    }
+}
+
+
+
+function validarCelular(celular) {
+    var myPhone = document.getElementById("mesageCellPhone");
+    var celularRegex = /^\d{2}\s\d{4,5}-\d{4}$/;
+
+    if (!celularRegex.test(celular)) {
+        myPhone.innerHTML = "Por favor, digite o seu celular...";
+        myPhone.style.color = "red";
+    } else {
+        myPhone.innerHTML = "Número válido valeu cara!!!!";
+        myPhone.style.color = "green"
+
+    }
+}
+
+
+function validarPassword() {
+
+    const password = document.querySelector(".password");
+    const msgPassword = document.getElementById("mensagemSenha");
+    const myPassword = password.value;
+
+    if (myPassword === "") {
+        msgPassword.innerHTML = "Por favor, preencha o campo de endereço...";
+        msgPassword.style.color = "red";
+    } else {
+        msgPassword.innerHTML = "Muito obrigedo teu certo beleza!!";
+        msgPassword.style.color = "green"
+    }
+}
+
+function validarConfirmPassword() {
+
+    const password = document.querySelector(".password");
+    const ConfirmPassword = document.querySelector(".userConfirmPassword");
+
+    const smsConfirmPassword = document.getElementById("msgConfirmPassword");
+
+    const myPassword = password.value;
+    const userConfirmPassword = ConfirmPassword.value;
+
+    if (userConfirmPassword === "") {
+        smsConfirmPassword.innerHTML = "Por favor, preencha o campo de cofirmacão de Senha...";
+        smsConfirmPassword.style.color = "red";
+    }
+
+    else if (userConfirmPassword != myPassword) {
+        smsConfirmPassword.innerHTML = "As senhas precisam ser iguais ta bom!!";
+        smsConfirmPassword.style.color = "red";
+    }
+
+    else {
+        smsConfirmPassword.innerHTML = "Muito obrigado deu certo beleza!!";
+        smsConfirmPassword.style.color = "green"
+    }
+}
+
+function validarEndereco() {
+
+    const enderecoInput = document.querySelector(".userAddress");
+    const msDeEndereco = document.getElementById("mensagemEdereco");
+    const endereco = enderecoInput.value;
+
+    if (endereco === "") {
+        msDeEndereco.innerHTML = "Por favor, preencha o campo de endereço.";
+        msDeEndereco.style.color = "red";
+    } else {
+        msDeEndereco.innerHTML = "Muito obrigado deu certo beleza!!";
+        msDeEndereco.style.color = "green"
+    }
 }
